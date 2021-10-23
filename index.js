d37c1ca4d71b716843fb1b389a469ff7a6b79f37
musasazib <musasazib@gmail.com>
2021-10-24 02:18:53 +0600

    Added Updated(PUT)

--- index.js
+++ index.js
@@ -48,6 +48,23 @@ async function run() {
       res.json(result);
     });
 
+    // Update API 
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      console.log('Updating user');
+      res.json(result);
+    })
+
     // Delete API
     app.delete('/users/:id', async(req, res) => {
       const id = req.params.id;
